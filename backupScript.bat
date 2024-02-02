e2b812552e24e12b0369b0e0aeaba72c49b8e638
Zachary Beyer <zack@boomlearning.com>
2024-02-01 17:54:58 -0700

    backup script complete (I think)

--- backupScript.bat
+++ backupScript.bat
@@ -3,20 +3,32 @@
 set source_dir=C:\SourceDirectory
 @REM set desination, where backups are store
 set destination_dir=D:\DestinationDirectory
-@REM TODO: set max size 
+@REM set max size 
+set max_value=5
 
 @REM make copy 
 xcopy "%source_dir%" "%destination_dir%" /s /e /y
 @REM go to destination
 cd %destination_dir%
 
-@REM Get directory listing in reverse order
+rem Increase folder names in reverse order
+for /f "tokens=*" %%F in ('dir /ad /b /o-n "%destination_dir%"') do (
+    set "folder_name=%%F"
+    
+    rem Extract the numeric part of the folder name
+    for /f "tokens=2 delims=Folder" %%N in ("%%F") do set /a "new_number=%%N + 1"
 
-@REM for /d %%F in ("%destination%\*") do (
-@REM     set /a index+=1
-@REM     set "folders[!index!]=%%F"
-@REM )
+    rem Construct the new folder name
+    set "new_name=!new_number!"
 
-@REM remame them
+    rem Rename the folder
+    ren "%destination_dir%\!folder_name!" "!new_name!"
 
-@REM delete the after max
+    rem Check the folder size and delete if it's above max
+    for /f %%A in ('dir /s /b "%destination_dir%\!new_name!" ^| find /c /v ""') do (
+        if %%A gtr %max_value% (
+            echo Deleting folder "!new_name!" as it's above 80.
+            rd /s /q "%destination_dir%\!new_name!"
+        )
+    )
+)
